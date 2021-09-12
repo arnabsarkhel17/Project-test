9d1848bca2f093b872852c133784b7df9dbc4227
Akshat1902 <akshatchopra1711@gmail.com>
2021-09-12 18:03:37 +0530

    updating commands

--- installJenkins.sh
+++ installJenkins.sh
@@ -5,7 +5,7 @@ java -version
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
-sudo apt install jenkins -Y
+sudo apt install jenkins -y
 sudo systemctl start jenkins
 sudo ufw allow 8080
 sudo ufw allow OpenSSH
