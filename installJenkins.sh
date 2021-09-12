44b34a17fc216ca14e91a7819357cd361a733e4a
Akshat1902 <akshatchopra1711@gmail.com>
2021-09-12 17:15:59 +0530

    K8s files and ansible files

---
+++ installJenkins.sh
@@ -0,0 +1,9 @@
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt install jenkins
+sudo systemctl start jenkins
+sudo ufw allow 8080
+sudo ufw allow OpenSSH
+sudo ufw enable
+
