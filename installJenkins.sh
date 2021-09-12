dd521b91e727f2d1f54c7979a6e51e325e19e56d
Akshat1902 <akshatchopra1711@gmail.com>
2021-09-12 17:57:26 +0530

    adding java install commands

--- installJenkins.sh
+++ installJenkins.sh
@@ -1,9 +1,13 @@
+sudo apt install default-jdk
+javac -version
+sudo apt install default-jre
+java -version
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
-sudo apt install jenkins
+sudo apt install jenkins -Y
 sudo systemctl start jenkins
 sudo ufw allow 8080
 sudo ufw allow OpenSSH
-sudo ufw enable
+sudo ufw enable -y
 
